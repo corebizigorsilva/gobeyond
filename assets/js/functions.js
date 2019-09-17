33a930f83b19c580fb6b3b6e51ed75be35077a98
anabialopes <lopesbeatrizw@gmail.com>
2019-09-17 20:00:25 -0300

    Criação de menu recursivo e Banners no slider

---
+++ assets/js/functions.js
@@ -0,0 +1,154 @@
+$(document).ready(function(){
+
+Menu.init();
+Banner.init();
+});
+
+let Menu = {
+    init: function(){
+
+        this.getMenuItens();
+        
+    },
+
+    getMenuItens: function(){
+        let _self = this;
+
+        $.get('http://localhost:5000/json/menu.json').then((response) => {
+            let test =  response.map((category) => {
+                let retorno = _self .listMenuItem(category);
+                return retorno;
+            });
+
+            $(".nav ul").append(test);
+     })
+            
+},
+
+    listMenuItem: function (tasty){
+        let _self = this;
+        let _html = "<li><a href='" + tasty.url + "'>" + tasty.name + "</a>";
+
+        if(tasty.children.length >0) {
+            let _childHtml = tasty.children.map((cat) => {
+                return _self.listMenuItem(cat);
+             });
+        
+             _html += "<div class='nav__dropdown'><ul>" + _childHtml + "</ul></div>"
+        };
+
+
+    _html  += "</li>";
+    return _html;
+
+    }
+}
+
+let Banner = {
+    init: function(){
+        let _self = this;
+        _self.getBanners();
+    },
+
+    getBanners: function(){
+        let _self = this;
+
+        $.get('http://localhost:5000/json/banner.json').then((response) => {
+            _self.listBanners(response);
+        });
+
+    },
+    listBanners: function(banners){
+        banners.map((response) => {
+            let html = `<div class="slide">
+            <div class="shell">
+                <div class="cols">
+                    <div class="col col--1of2">
+                        <h1>Verdadeira Tradição corebiz</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut</p>
+
+                        <a href="#" class="btn btn--red">
+                            <span>VER A COLEÇÃO</span>
+
+                            <i class="ico-dot"></i>
+                        </a>
+                    </div><!-- /.col col-/-1of2 -->
+
+                    <div class="col col--1of2">
+                        <img src="assets/images/temp/slider-image1.jpg" alt="" class="hidden-xs hidden-sm">
+
+                        <img src="assets/images/temp/slider-image1-mobile.jpg" alt="" class="hidden-md hidden-lg">
+                    </div><!-- /.col col-/-1of2 -->
+                </div><!-- /.cols -->
+            </div><!-- /.shell -->
+        </div><!-- /.slide -->`
+
+        $('.slides').append(html);
+    });
+
+    sliderInit();
+  }
+}
+
+function sliderInit() {
+    $('.slider .slides').slick({
+        dots: true,
+        arrows: false
+    });
+    $('.slider-product .slides').slick({
+        dots: true,
+        arrows: false
+    });
+    $('.slider-featured .slides').slick({
+        dots: true,
+        arrows: false,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                infinite: true
+            }
+        }]
+    });
+    $('.list-gallery').each(function () {
+        var $this = $(this);
+        var slidesNumber = $this.data('slides-mobile');
+        $this.slick({
+            dots: false,
+            arrows: true,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            infinite: false,
+            responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: slidesNumber,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false,
+                    arrows: true
+                }
+            }]
+        });
+    });
+    imageZoom();
+}
+
+function imageZoom() {
+    $('.product__zoom').each(function () {
+        var $this = $(this);
+        var $image = $this.closest('.product__image');
+        var $imageContainer = $image.find('.product__image-zoom');
+        var imgURL = $this.data('image');
+        $this.zoom({
+            on: 'click',
+            url: imgURL,
+            target: $imageContainer,
+            onZoomIn: function onZoomIn() {
+                $image.addClass('active');
+            },
+            onZoomOut: function onZoomOut() {
+                $image.removeClass('active');
+            }
+        });
+    });
+}
