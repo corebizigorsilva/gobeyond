22353b655d030e68c1d06e4ae92af416d39a8383
name <kauan.gomes@corebiz.com.br>
2019-09-18 19:44:00 -0300

    Aplicação da vitrine e formatação de texto

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 
     Menu.init();
     Banner.init();
+    vitrine.init();
 
 })
 
@@ -85,12 +86,91 @@ let Banner = {
                 </div><!-- /.shell -->
             </div><!-- /.slide -->`
 
-            $('.slides').append(html);
+            $('.slides-banner').append(html);
         });
 
         sliderInit();
     }
 }
+
+let vitrine ={
+    init: function(){
+        
+    this.getvitrine();
+
+    },
+    getvitrine: function(){
+        let _self = this;
+        $.get('http://localhost:5000/json/vitrine.json').then((response) =>{ //Está efetuando a busca da vitrine no local indicado
+            _self.listVitrine(response);
+        });
+    },
+
+    listVitrine: function(items){
+        //sku - variação
+        //sellers é a propria fila
+        let _self = this;
+
+        let _htmlVitrines = items.map((item) => {
+            let skusAvailable = item.items.filter((sku) => {
+                if(sku.sellers[0].commertialOffer.AvailableQuantity > 0){ //Condição para validação de busca na sku
+                    return sku;
+                }              
+            });
+
+            
+             let price = _self.formatMoney(skusAvailable[0].sellers[0].commertialOffer.Price);
+             let listPrice =  _self.formatMoney(skusAvailable[0].sellers[0].commertialOffer.ListPrice);
+           
+            let _html = `<div class="col col--1of3">
+            <div class="product">
+                <div class="product__image">
+                    <a href="#">
+                        <img src="`+item.items[0].images[0].imageUrl+`" alt="">
+                    </a>
+                </div><!-- /.product__image -->
+        
+                <div class="product__content">
+                    <p>`+skusAvailable.length+` possibilidades</p>
+        
+                    <h3>
+                        <a href="#">`+ price +`</a>
+                    </h3>
+        
+                    <ul class="list-price">
+                        <li>
+                            <del>`+ listPrice +`</del>
+                        </li>
+        
+                        <li>
+                            <strong>`+ price +`</strong>
+                        </li>
+                    </ul><!-- /.list-price -->
+
+                    <a href="`+ skusAvailable[0].sellers[0].addToCartLink +`" target="_blank"> Comprar </a>
+                                        
+                </div><!-- /.product__content -->
+            </div><!-- /.product -->
+        </div><!-- /.col col-/-1of3 -->`;
+
+        return _html;
+        });
+
+        $('.slide-vitrines').append(_htmlVitrines);
+
+    },
+    formatMoney: function(valor){
+       
+         valor = valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+
+         return valor;
+        
+        
+    }
+}
+
+
+
 
 function sliderInit() {
     $('.slider .slides').slick({
@@ -134,8 +214,6 @@ function sliderInit() {
     });
     imageZoom();
 }
-
-
 
 function imageZoom() {
     $('.product__zoom').each(function () {
