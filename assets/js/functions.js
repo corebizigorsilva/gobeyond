8a8cae1d57d9b0aaa72fa9d9e7b4388272c15402
marjoritamise <marjori.tamise@gmail.com>
2019-09-18 19:44:17 -0300

    Aplicação da vitrine e formatação de preço

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     Menu.init();
     Banner.init();
+    Vitrine.init();
 });
 
 let Menu = {
@@ -77,11 +78,104 @@ let Banner = {
                 </div><!-- /.shell -->
             </div><!-- /.slide -->`
 
-            $('.slides').append(html);
+            $('.slides-banner').append(html);
 
         });
 
         sliderInit();
+    }
+}
+
+let Vitrine = {
+    init: function () {
+        this.getVitrine();
+    },
+    getVitrine: function () {
+        let _self = this;
+        $.get('http://localhost:5000/json/vitrine.json').then((response) => {
+            _self.listVitrine(response);
+        });
+    },
+    listVitrine: function (items) {
+        let _self = this;
+
+        let _htmlVitrines = items.map((item) => {
+            let skusAvailable = item.items.filter((sku) => {
+                if (sku.sellers[0].commertialOffer.AvailableQuantity > 0) {
+                    return sku;
+                }
+            });
+
+            let price = _self.formatMoney(skusAvailable[0].sellers[0].commertialOffer.Price);
+            let listPrice = _self.formatMoney(skusAvailable[0].sellers[0].commertialOffer.ListPrice);
+
+            let _html = `<div class="col col--1of3">
+                            <div class="product">
+                                <div class="product__image">
+                                    <a href="#">
+                                        <img src="` + item.items[0].images[0].imageUrl + `" alt="">
+                                    </a>
+                                </div>                        
+                                <div class="product__content">
+                                    <p>` + skusAvailable.length + ` possibilidades</p>
+                                    <h3>
+                                        <a href ="#"> ` + price + ` </a>
+                                    </h3>
+                        
+                                    <ul class="list-price">
+                                        <li>
+                                            <del> ` + listPrice + ` </del>
+                                        </li>
+                        
+                                        <li>
+                                            <strong> ` + price + ` </strong>
+                                        </li>
+                                    </ul>
+
+                                    <a href="` + skusAvailable[0].sellers[0].addToCartLink + `" target="_blank"> Comprar </a>
+                                </div>
+                            </div>
+                        </div>`;
+
+            return _html;
+        });
+
+        $('.slide-vitrines').append(_htmlVitrines);
+    },
+
+    // Eugenio
+    //formatMoney: function (valor) {
+    //     valor = `R$ ${parseFloat(valor).toFixed(2).replace(".", ",")}`;
+    //     return valor;
+    // }
+
+    //João
+    // formatMoney: function (valor) {
+    //     valor = valor.toLocaleString('pt-BR', {
+    //         style: 'currency',
+    //         currency: 'BRL'
+    //     });
+    //     return valor
+    // }
+
+    //Gabrielly
+    // formatMoney: function (valor) {
+    //     valor = "R$ " + valor.toFixed(2).replace(".", ",");
+    //     return valor;
+    // }
+
+    //Priscila
+    // formatMoney: function (valor) {
+    //     return 'R$' + valor.toFixed(2);
+    // }
+
+    formatMoney: function (valor) {
+
+        return valor.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        });
+
     }
 }
 
